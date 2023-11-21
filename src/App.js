e3009d495b0e252bfe57df4b3285e7bac0aea769
Roshni Syed <105354040+theroshni01@users.noreply.github.com>
2023-11-21 11:13:38 +0530

    Uploaded

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+         <h1 data-clr="clr">Hello World</h1>
+         <h2>Welcome to React App</h2>
+    </div>
+  );
+}
+
+export default App;
